4d8d32dc086e135a434b581a1dafc459bd6e52e8
dlrjsud <rnjsguddlwkd@gmail.com>
2023-02-26 15:18:55 +0900

    3장 연습

---
+++ chapter03/chapter3_3.py
@@ -0,0 +1,15 @@
+# 연산자의 종류
+num1 = 10
+num2 = 5
+print('num1 + num2는', num1 + num2, '입니다.')
+print('num1 - num2는', num1 - num2, '입니다.')
+print('num1 * num2는', num1 * num2, '입니다.')
+print('num1 / num2는', num1 / num2, '입니다.')
+print('num1 % num2는', num1 % num2, '입니다.')
+
+# 문자열을 조작하는 연산자
+num = 10
+pic = 'O'  # *연산자로 문자열을 정수배로 지정해 반복
+graph = pic * num  # +연산자로 문자열끼리 연결
+print('판매 금액:', graph)
+print(num, '만 원의 매출액이 있습니다.')
