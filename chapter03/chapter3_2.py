4385ad07ca5560e3107bc95653f58f42adbac300
dlrjsud <rnjsguddlwkd@gmail.com>
2023-02-26 15:03:00 +0900

    3장 연습

---
+++ chapter03/chapter3_2.py
@@ -0,0 +1,16 @@
+# 식의 구조           1(오퍼랜드)+2(오퍼랜드)=3(식)
+
+# 식의 값 출력
+print('1+2는', 1 + 2, '입니다')
+
+# 변수 연산
+price = 50
+num = 10
+total = price * num  #price와 num 값을 *한것을 total에 대입
+print('단가는', price, '원입니다.')
+print('판매개수는', num, '개입니다.')
+print('합계 급액은', total, '원입니다.')
+
+total = total - 100   #total에서 100을 -것을 total에 대입
+
+print('할인하면', total, '원입니다.')
