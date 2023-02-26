ee620cf8f26f6746e799678f854d5028ac85a51c
dlrjsud <rnjsguddlwkd@gmail.com>
2023-02-26 16:04:29 +0900

    3장 연습

---
+++ chapter03/chapter3_6.py
@@ -0,0 +1,9 @@
+# 연습문제
+
+num = int(input('당신은 몇 살입니까?'))
+print('당신은', num, '살입니다.')
+
+num1 = float(input('키를 입력하세요.'))
+num2 = float(input('몸무게를 입력하세요.'))
+print('키는',num1,'센티미터입니다.')
+print('몸무게는',num2,'킬로그램입니다.')
