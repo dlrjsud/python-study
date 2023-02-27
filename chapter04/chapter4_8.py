4834c416b22f4daf2a60a920f5f833763262886a
dlrjsud <rnjsguddlwkd@gmail.com>
2023-02-27 21:23:58 +0900

    4쟝 연습

--- chapter04/chapter4_8.py
+++ chapter04/chapter4_8.py
@@ -1,14 +1,17 @@
+1.
 print('1부터 10까지의 짝수를 표시하세요.')
 
 for i in range(10):
     if (i + 1) % 2 == 0:
         print(i + 1)
 
+#2.
 print('1부터 10까지의 짝수를 표시하세요.')
 
 for i in range(2, 11, 2):
     print(i)
 
+#3.
 for i in range(1, 10):
     for j in range(1, 10):
         print(i * j, '\t', end='')
@@ -16,6 +19,7 @@ for i in range(1, 10):
 
 v = False
 
+#4.
 for i in range(1, 6):
     for j in range(i):
         print('*', end='')
